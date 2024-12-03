9a5f8637538a5f88344ff7e81794efef21477e04
Chenggong <chenggong.hui@mpinat.mpg.de>
2024-12-03 16:16:15 +0100

    test example for HRE

---
+++ grand/tests/test_samplers_Multi.py
@@ -0,0 +1,15 @@
+import unittest
+
+import numpy as np
+from openmm import app, unit, openmm
+
+from grand import samplers
+
+
+class TestNonequilibriumGCMCSphereSamplerMultiState(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
