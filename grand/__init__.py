7fe5bec9a7c81944efd701c5e44c93322c865098
Chenggong <chenggong.hui@mpinat.mpg.de>
2024-12-03 22:30:53 +0100

    start 1.2.2_dev

--- grand/__init__.py
+++ grand/__init__.py
@@ -4,10 +4,10 @@ grand
 OpenMM-based implementation of grand canonical Monte Carlo (GCMC) moves to sample water positions
 
 Marley L. Samways
-Chenggong
+Chenggong Hui
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2_dev"
 
 from grand import samplers, utils, potential, tests
 
